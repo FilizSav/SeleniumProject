c19a38e27cff119179e85eb57bee29615e9d70b0
filizsav <filizsav@gmail.com>
2023-01-11 18:13:21 -0600

    Project1

---
+++ src/Project_01/TestCase2.java
@@ -0,0 +1,28 @@
+package Project_01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import util.Driver;
+
+import java.util.List;
+
+public class TestCase2 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = Driver.getDriver();
+
+        driver.get("https://automationexercise.com/");
+
+        Thread.sleep(10);
+        List<WebElement> elements = driver.findElements(By.cssSelector("ul[class*='nav navbar-nav'"));
+
+        for (WebElement e : elements){
+            if(e.isDisplayed()) System.out.println("Display Validation Passed");
+            if(e.isEnabled()) System.out.println("Enabled Validation Passed");
+            System.out.println(e.getText());
+        }
+
+        Driver.quitDriver();
+    }
+}
---
+++ src/util/Driver.java
@@ -0,0 +1,23 @@
+package util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+
+    public Driver(){}
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver", "/Users/filizsav/IdeaProjects/selenium_initial_project_5/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static void quitDriver(){
+        driver.quit();
+    }
+
+}
